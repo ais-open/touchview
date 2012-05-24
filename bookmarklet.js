0c2ed4352157dbb2504c1f376fa5a88e7fef26e6
Ian Gilman <ian@iangilman.com>
2012-05-24 16:07:34 -0700

    Updated the copyright on the bookmarklet

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
-// Copyright (c) 2012
-// Applied Information Sciences, http://ideas.appliedis.com/
+// Copyright (c) 2012 Applied Information Sciences, Inc.
+// http://www.appliedis.com
 //  
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
